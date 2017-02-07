eebdc66e2410b252b375cea3a7266b95b9f53ba1
MrFluffyPoPoken <matthew.bracken@hope.edu>
2017-02-07 13:26:05 -0500

    Update Compare.rb

--- Compare.rb
+++ Compare.rb
@@ -1,5 +1,4 @@
 require 'spreadsheet'
-require 'fileutils'
 
 Spreadsheet.client_encoding = 'UTF-8'
 
@@ -7,28 +6,30 @@ book1 = Spreadsheet.open 'C:\Users\brackm1\Documents\Ruby Tools\Portfolio Page C
 book2 = Spreadsheet.open 'C:\Users\brackm1\Documents\Ruby Tools\Portfolio Page Comparator\IL_NAICOA_20170729_293_2.xls' 
 
 def compareSheet sheet1, sheet2
+	@rowCount = 1
 	sheet1.rows.each_with_index do |row1, i|
 		row2 = sheet2.row i
-		compareRow(row1, row2)
+		compareRow(row1, row2, sheet2)
+		@rowCount += 1
 	end
 end
 
-def compareRow row1, row2
+
+
+def compareRow row1, row2, sheet2
 	notMatchingFormat = Spreadsheet::Format.new(border: :thin, border_color: :red)
 	row1.each_with_index do |cell1, i|
 		cell2 = row2[i]
-		unless cell1.to_s == cell2.to_s
-			row2.set_format(i, notMatchingFormat)
-			puts row2
+		if cell1.to_s != cell2.to_s
+			puts "#{sheet2.name}   ---   Column #{i + 1}   ---   Row #{@rowCount}   -->   #{row2[i]}"
 		end
 	end
 end
 
-#Changed so that sheet 2 is a clone of the original, and is a new object instead of a reference. 
 book1.worksheets.each_with_index do |sheet1, i|
 	s2 = book2.worksheet i
 	sheet2 = s2.clone
 	compareSheet(sheet1, sheet2)
 end
 
-book2.write 'C:\Users\brackm1\Documents\Ruby Tools\Portfolio Page Comparator\IL_NAICOA_20170729_293_compare.xls'
+#book2.write 'C:\Users\brackm1\Documents\Ruby Tools\Portfolio Page Comparator\IL_NAICOA_20170729_293_compare.xls'
